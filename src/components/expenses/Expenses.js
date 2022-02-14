8c6c27caa04815ca9e5fda1ecd2baf0b9f6098d3
Anton Guinto <antonvinceguinto@gmail.com>
2022-02-14 20:06:20 +0800

    feat: Add year filter

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,12 +1,37 @@
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
-import Card from '../global/Card';
+import Card from '../global/Card'
+import ExpensesFilter from '../expense-filter/ExpensesFilter'
+import { useState } from 'react'
 
 const Expenses = (props) => {
+	const [ filteredYear, setFilteredYear ] = useState('2022')
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear)
+	}
+
+	const filteredExpenses = props.items.filter((e) => {
+		return e.date.getFullYear().toString() === filteredYear
+	})
+
 	return (
-		<Card className='expenses'>
-			{props.items.map((e) => <ExpenseItem key={e.id} title={e.title} date={e.date} amount={e.amount} />)}
-		</Card>
+		<div>
+			<Card className='expenses'>
+				<ExpensesFilter selected={filteredYear} onFilter={filterChangeHandler} />
+				{props.items.length <= 0 ? (
+					<center>
+						<br />
+						<p style={{ color: 'white' }}>Welcome! Start adding your expenses🛒</p>
+						<br />
+					</center>
+				) : (
+					filteredExpenses.map((e) => (
+						<ExpenseItem key={e.id} title={e.title} date={e.date} amount={e.amount} />
+					))
+				)}
+			</Card>
+		</div>
 	)
 }
 
